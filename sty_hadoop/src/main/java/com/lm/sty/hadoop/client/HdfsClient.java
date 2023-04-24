b32869415bbb4fbd7716bb858ba35cf57873723b
‘lianyutian’ <1436159527@qq.com>
2023-04-24 17:09:23 +0800

    添加hdfs笔记

--- sty_hadoop/src/main/java/com/lm/sty/hadoop/client/HdfsClient.java
+++ sty_hadoop/src/main/java/com/lm/sty/hadoop/client/HdfsClient.java
@@ -27,6 +27,11 @@ public class HdfsClient {
         fs.mkdirs(new Path("/sanguozhanji2"));
     }
 
+    /**
+     * 文件上传
+     *
+     * @throws IOException
+     */
     @Test
     public void testCopyFromLocalFile() throws IOException {
         Configuration configuration = new Configuration();
@@ -36,6 +41,30 @@ public class HdfsClient {
         fileSystem.copyFromLocalFile(
                 new Path("/home/liming/develop_tools/maven/apache-maven-3.8.6/conf/settings.xml"),
                 new Path("/settings.xml"));
+
+        fileSystem.close();
+    }
+
+    /**
+     * 文件下载
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testCopyToLocalFile() throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", "hdfs://192.168.1.201:9820");
+        FileSystem fileSystem = FileSystem.get(configuration);
+
+        // boolean delSrc 指是否将原文件删除
+        // Path src 指要下载的文件路径
+        // Path dst 指将文件下载到的路径
+        // boolean useRawLocalFileSystem 是否开启文件校验
+        fileSystem.copyToLocalFile(false,
+                new Path("/settings.xml"),
+                new Path("/home/liming/develop_tools/maven/apache-maven-3.8.6/conf/settings.xml"),
+                true
+        );
 
         fileSystem.close();
     }
